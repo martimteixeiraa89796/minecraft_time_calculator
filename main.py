da8ea0e36f0a462874355d7942ea4a5082c61067
martimteixeiraa89796 <a89796@ualg.pt>
2026-07-23 11:47:03 +0100

    Simplified wrap around time calculation, including night time lenght

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Real life
 day_total_seconds = 24 * 60*60 #Hours in a day * seconds in an hour
-time_to_calculate = "10:39:00"
+time_to_calculate = "23:39:00"
 sunrise = "05:11:00"
 sunset = "21:01:00"
 sunrise_tomorrow = "05:12:00"
@@ -44,7 +44,7 @@ sunset = convert_to_seconds(convert_to_time_object(sunset))
 sunrise_tomorrow = convert_to_seconds(convert_to_time_object(sunrise_tomorrow))
 sunset_yesterday = convert_to_seconds(convert_to_time_object(sunset_yesterday))
 
-minecraft_day_duration = ((minecraft_sunset_tick - minecraft_sunrise_tick) + minecraft_day_total_ticks) % minecraft_day_total_ticks
+minecraft_day_duration = (minecraft_sunset_tick - minecraft_sunrise_tick) % minecraft_day_total_ticks
 minecraft_night_duration = minecraft_day_total_ticks - minecraft_day_duration
 
 
@@ -91,10 +91,13 @@ else:
     else: #Before midnight
         sunrise = sunrise_tomorrow  
     
+    #This is a reference to an old method that was used to calculate the night lenght
     #Think as a circle, where midnight is the origin. It takes the entire time from 0 to sunset.
     #What is left is a tiny slice of that circle, from sunset to midnight. We add the time untill sunrise to that.
-    night_duration = (day_total_seconds - sunset) + sunrise
-    elapsed_time = (time_to_calculate - sunset + day_total_seconds) % day_total_seconds
+    #night_duration = (day_total_seconds - sunset) + sunrise #Old way
+    
+    night_duration = (sunrise - sunset) % day_total_seconds
+    elapsed_time = (time_to_calculate - sunset) % day_total_seconds
     progress = elapsed_time / night_duration
     
     minecraft_time = minecraft_sunset_tick + (progress * minecraft_night_duration)
