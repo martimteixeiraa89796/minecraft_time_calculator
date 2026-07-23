9a34380f0fb452c3d405e932e24a352887276572
martimteixeiraa89796 <a89796@ualg.pt>
2026-07-23 12:12:36 +0100

    Added file with configurations

---
+++ config.py
@@ -0,0 +1,13 @@
+#Real life
+day_total_seconds = 24 * 60*60 #Hours in a day * seconds in an hour
+time_to_calculate = "23:39:00"
+sunrise = "05:11:00"
+sunset = "21:01:00"
+sunrise_tomorrow = "05:12:00"
+sunset_yesterday = "21:03:00"
+moonphase = "Waxing Gibbous"
+
+#Minecraft
+minecraft_day_total_ticks = 24000
+minecraft_sunrise_tick = 23000
+minecraft_sunset_tick = 13000
